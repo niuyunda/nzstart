11e54d7df470dc76fb25316f175bb3a99fe4afd9
niuyunda <niuyunda@gmail.com>
2025-03-09 10:16:52 +0000

    编辑地址错误修复

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/niuyunda/nzstart/tree/main/docs',
+            'https://github.com/niuyunda/nzstart/tree/main',
         },
         blog: {
           showReadingTime: true,
