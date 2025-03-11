826cbf88a549b346b1617d4fe9402b8fcd64d987
niuyunda <niuyunda@gmail.com>
2025-03-11 05:01:47 +0000

    docs目录直接导航到欢迎页面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/欢迎">
+            to="/docs">
             开始阅读
           </Link>
         </div>
