90cdc8f2e670bd81b338791acd7c3f44956e8ca0
niuyunda <niuyunda@gmail.com>
2025-03-11 05:10:58 +0000

    删除了编辑此页的链接

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/niuyunda/nzstart/tree/main',
+          // editUrl:
+          //   'https://github.com/niuyunda/nzstart/tree/main',
         },
         blog: {
           showReadingTime: true,
@@ -50,8 +50,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/niuyunda/nzstart/tree/main/blog',
+          // editUrl:
+          //   'https://github.com/niuyunda/nzstart/tree/main/blog',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
