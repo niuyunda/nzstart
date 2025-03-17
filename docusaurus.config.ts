872843fe7bb4938eb07ee7745a3f46e3cceabbb3
niuyunda <niuyunda@gmail.com>
2025-03-17 09:18:47 +0000

    增加 vercel-analytics 和 sitemap 插件

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,29 @@ const config: Config = {
         googleTagManager: {
           containerId: 'GTM-PQT6GB7H',
         },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
       } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    [
+      'vercel-analytics',
+      {
+        debug: true,
+        mode: 'auto',
+      },
     ],
   ],
 
